fb7c5d81f5c3b03f6fb739392ddd71522f542c2a
Felipe Sales <sales.felipeb@gmail.com>
2017-08-07 21:42:06 -0300

    Nova versão fazendo merge dos arquivos do output

--- src/main/java/com/nSearch/Searcher.java
+++ src/main/java/com/nSearch/Searcher.java
@@ -4,80 +4,151 @@ package com.nSearch;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-import java.io.IOException;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Searcher extends Configured implements Tool {
 
-    public static class MapperClass extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+    private final String URL_LOCAL = "http://localhost:5000/results";
+    private final String URL_DEV =   "http://yinyang.herokuapp.com/results";
 
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+
+    public static class MapperClass extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+
+        private Text resultId = new Text();
+        private Text keyText = new Text();
 
         private static String searchKey;
 
         public void configure(JobConf job) {
-            searchKey = job.get("name");
+            searchKey = job.get("keyword");
         }
 
-        public void map(LongWritable key, Text value,
-                        OutputCollector<Text, IntWritable> output,
-                        Reporter reporter) throws IOException {
+        public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
             String line = value.toString();
-            String id, conteudo;
+            String id;
 
             StringTokenizer itr = new StringTokenizer(line,",");
 
             id = itr.nextToken();
-            conteudo = itr.nextToken();
 
-            if((conteudo.toLowerCase()).contains(searchKey.toLowerCase())) {
-                word.set(id);
-                output.collect(word, one);
+            if((line.toLowerCase()).contains(searchKey.toLowerCase())) {
+                resultId.set(id);
+                keyText.set(searchKey);
+                output.collect(keyText,resultId);
             }
         }
     }
 
-    public static class ReducerClass extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+    public static class ReducerClass extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
 
-        public void reduce(Text key, Iterator<IntWritable> values,
-                           OutputCollector<Text, IntWritable> output,
-                           Reporter reporter) throws IOException {
-            int sum = 0;
+        public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+            String result = "";
+            Text resultT = new Text();
+            boolean first = true;
             while (values.hasNext()) {
-                sum += values.next().get();
+                if(first) {
+                    result += values.next();
+                    first = false;
+                } else {
+                    result += "," + values.next();
+                }
             }
-            output.collect(key, new IntWritable(sum));
+
+            resultT.set(result);
+
+            output.collect(key, resultT);
         }
     }
 
     private static int printUsage() {
-        System.out.println("nSearch [-m <maps>] [-r <reduces>] <input> <output>");
+        System.out.println("nSearch [-m <maps>] [-r <reduces>] <input> <output> <keyword>");
         ToolRunner.printGenericCommandUsage(System.out);
         return -1;
     }
 
+    private void sendResult(String result, String userId, String selectedUrl) throws IOException {
+
+        String postData = "{ \"data\": \"" + result + "\" , \"id\": \"" + userId + "\" }";
+
+        System.out.println(String.format("Post data: %s",postData));
+
+        URL url = new URL(selectedUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Content-Length",  String.valueOf(postData.length()));
+
+        // Write data
+        OutputStream os = connection.getOutputStream();
+        os.write(postData.getBytes());
+
+        // Read response
+        StringBuilder responseSB = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String line;
+        while ( (line = br.readLine()) != null)
+            responseSB.append(line);
+
+        // Close streams
+        br.close();
+        os.close();
+
+        System.out.println("Envio concluido!");
+    }
+
+    private String retrieveResult (String outputPath) throws IOException, InterruptedException {
+        String result = "";
+        String command = String.format("hdfs dfs -cat %s/part-* ", outputPath);
+
+        Runtime rt = Runtime.getRuntime();
+        Process pr = rt.exec(command);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+
+        String line;
+        while ((line = reader.readLine()) != null)
+            result += line;
+
+        pr.waitFor();
+
+        String[] sub = result.split("\t");
+
+        result = sub[sub.length - 1];
+
+        return result;
+    }
 
     public int run(String[] args) throws Exception {
+
+        final long startTime, endTime;
+        String results, selectedUrl;
+
+        //////////// Arguments ///////////
+        String outputArg, localArg, inputArg, userIdArg, keywordArg;
+
+        startTime = System.currentTimeMillis();
+
         JobConf conf = new JobConf(getConf(), Searcher.class);
         conf.setJobName("nSearch");
 
-        // the keys are words (strings)
         conf.setOutputKeyClass(Text.class);
-        // the values are counts (ints)
-        conf.setOutputValueClass(IntWritable.class);
-
+        conf.setOutputValueClass(Text.class);
         conf.setMapperClass(MapperClass.class);
         conf.setCombinerClass(ReducerClass.class);
         conf.setReducerClass(ReducerClass.class);
@@ -102,21 +173,60 @@ public class Searcher extends Configured implements Tool {
             }
         }
 
-        // Make sure there are exactly 2 parameters left.
-//        if (other_args.size() != 2) {
-//            System.out.println("ERROR: Wrong number of parameters: " +
-//                    other_args.size() + " instead of 2.");
-//            return printUsage();
-//        }
+        //////////// Retrieving arguments ///////////
+        if (other_args.size() != 5) {
+            System.out.println("ERROR: Wrong number of parameters: " +
+                    other_args.size() + " instead of 5.");
+            return printUsage();
+        }
 
-        FileInputFormat.setInputPaths(conf, other_args.get(0));
-        FileOutputFormat.setOutputPath(conf, new Path(other_args.get(1)));
-        conf.set("name",other_args.get(2));
+        inputArg   = other_args.get(0);
+        outputArg  = other_args.get(1);
+        keywordArg = other_args.get(2).replaceAll("%"," ");
+        userIdArg  = other_args.get(3);
+        localArg   = other_args.get(4);
+
+        if((new String("dev")).equals(localArg))
+        {
+            selectedUrl = URL_DEV;
+        } else {
+            selectedUrl = URL_LOCAL;
+        }
+        //////////////////////////////////////////
+
+        Path outputPath = new Path(outputArg);
+        FileInputFormat.setInputPaths(conf, inputArg);
+        FileOutputFormat.setOutputPath(conf, outputPath);
+
+        conf.set("keyword",keywordArg);
 
         JobClient.runJob(conf);
+
+        results = retrieveResult(outputArg);
+
+        System.out.println(String.format("Url selecionada: %s", selectedUrl));
+        System.out.println(String.format("User id: %s", userIdArg));
+        System.out.println(String.format("Palavra chave: %s", keywordArg));
+        System.out.println(String.format("Resultado final: %s", results));
+
+        try {
+            System.out.println("Enviando a mensagem");
+            sendResult(results, userIdArg, selectedUrl);
+        } catch (Exception e) {
+            System.out.println("Erro ao enviar a mensagem!");
+
+            System.out.println(e.getMessage());
+        } finally {
+            outputPath.getFileSystem(conf).delete(outputPath,true);
+        }
+
+        endTime = System.currentTimeMillis();
+
+        System.out.println("Finalizando");
+        System.out.println("Total execution time: " + (endTime - startTime)/1000 + " seg" );
+
         return 0;
     }
-
 
     public static void main(String[] args) throws Exception {
         int res = ToolRunner.run(new Configuration(), new Searcher(), args);
